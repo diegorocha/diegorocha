ea06d43fc009776e7ba9f28135e1a29d9c26a46c
Diego Rocha <diego@diegorocha.com.br>
2016-11-09 02:23:08 -0200

    Implementado cache

--- diegorocha/curriculo/views.py
+++ diegorocha/curriculo/views.py
@@ -5,8 +5,16 @@ from django.http import JsonResponse
 from diegorocha.curriculo import models
 from django.shortcuts import get_object_or_404
 from django.core.mail import EmailMessage
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+default_cache = cache_page(60 * 30)
+
+
+@method_decorator(default_cache, name='dispatch')
+@method_decorator(ensure_csrf_cookie, name='dispatch')
 class HomeView(generic.TemplateView):
     template_name = 'home.html'
 
@@ -33,6 +41,8 @@ class SendContactView(generic.View):
         return JsonResponse(response)
 
 
+@method_decorator(default_cache, name='dispatch')
+@method_decorator(ensure_csrf_cookie, name='dispatch')
 class NotFoundView(generic.View):
     template_name = 'not-found.html'
 
