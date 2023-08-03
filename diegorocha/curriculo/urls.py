e53e07ee71493b82b60359094387d8bcb930a6de
Diego Rocha <diego.rocha@m4u.com.br>
2023-08-03 20:59:11 -0300

    Removing django files (deprecated)

--- diegorocha/curriculo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from diegorocha.curriculo import views
-
-app_name = 'curriculo'
-urlpatterns = [
-    url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^contact/$', views.SendContactView.as_view(), name='send-contact'),
-    url(r'^healthcheck/$', views.HealthCheckView.as_view(), name='healthcheck'),
-]
